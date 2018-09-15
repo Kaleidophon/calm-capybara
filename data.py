0d6456a509604151f3122c390098f07eccee4ded
Daniel <dfdazac@gmail.com>
2018-09-15 10:34:39 +0200

    Store text ids when loading

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ import nltk
 from collections import Counter, defaultdict
 from sklearn.feature_extraction.text import TfidfTransformer
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import lil_matrix
 
 TEXT_EXT = '.text'
 LABELS_EXT = '.labels'
@@ -27,6 +27,7 @@ class TweetsBaseDataset(data.Dataset):
         self.length = 0
 
         # Open text file with tweets
+        print('Reading file...')
         with open(os.path.join(path, prefix + TEXT_EXT)) as file:
             for i, line in enumerate(file):
                 self.length += 1
@@ -35,15 +36,23 @@ class TweetsBaseDataset(data.Dataset):
                 token_counts.update(tokens)
                 processed_tweets.append(tokens)
 
-        # Build vocabulary and store words as integers
-        self.vocabulary = defaultdict(lambda: len(self.vocabulary))
-        _ = self.vocabulary[PAD_SYMBOL]
-        unk_idx = self.vocabulary[UNK_SYMBOL]
+        # Build vocabulary
+        print('Building vocabulary...')
+        vocabulary = defaultdict(lambda: len(vocabulary))
+        _ = vocabulary[PAD_SYMBOL]
+        unk_idx = vocabulary[UNK_SYMBOL]
         for token, _ in token_counts.most_common(vocab_size):
-            _ = self.vocabulary[token]
-        self.vocabulary.default_factory = lambda: unk_idx
+            _ = vocabulary[token]
+        self.vocabulary = dict(vocabulary)
+
+        # Store text in memory as word ids
+        self.text_ids = []
+        for tweet in processed_tweets:
+            self.text_ids.append(
+                list(map(lambda x: self.vocabulary.get(x, unk_idx), tweet)))
 
         # Load labels
+        print('Loading labels...')
         self.labels = np.empty(self.length, dtype=np.int)
         with open(os.path.join(path, prefix + LABELS_EXT)) as file:
             for i, line in enumerate(file):
@@ -61,6 +70,7 @@ class TweetsBaseDataset(data.Dataset):
             - text (str): a raw tweet in string format
         Returns: list, containing tokens after processing
         """
+        # More operations can be added here before returning list of tokens
         return nltk.word_tokenize(text)
 
 class TweetsBOWDataset(TweetsBaseDataset):
@@ -74,18 +84,19 @@ class TweetsBOWDataset(TweetsBaseDataset):
     def __init__(self, path, prefix, vocab_size=10000):
         TweetsBaseDataset.__init__(self, path, prefix, vocab_size)
 
-        # Using the vocabulary, build count matrix from text
-        self.counts = np.zeros((self.length, len(self.vocabulary)), dtype=np.int)
-        with open(os.path.join(path, prefix + TEXT_EXT)) as file:
-            for i, line in enumerate(file):
-                # Count words in processed tweet
-                token_counter = Counter(self.process_tweet(line))
-                tokens, counts = zip(*token_counter.items())
-                token_idx = [self.vocabulary[token] for token in tokens]
-                self.counts[i, token_idx] = counts
+        # Using the vocabulary, build count matrix from text ids
+        print('Loading counts matrix...')
+        count_matrix = lil_matrix((self.length, len(self.vocabulary)),
+                                  dtype=np.int)
+        for i, token_ids in enumerate(self.text_ids):
+            # Count ids in tweet
+            token_id_counter = Counter(token_ids)
+            token_ids, counts = zip(*token_id_counter.items())
+            count_matrix[i, token_ids] = counts
 
         # Add tf-idf weighting
-        self.data = TfidfTransformer().fit_transform(self.counts)
+        print('Creating TF-ID matrix...')
+        self.data = TfidfTransformer().fit_transform(count_matrix)
 
 if __name__ == '__main__':
-    ds = TweetsBOWDataset('data/dev', 'test_trial')
+    ds = TweetsBOWDataset('data/dev', 'us_trial')
