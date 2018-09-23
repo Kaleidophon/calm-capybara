f64d5a0b2309b38c56f635fac948bb7d657e8e0b
Kaleidophon <Dennis.Ulmer@gmx.de>
2018-09-23 21:51:59 +0200

    Refining losswise session class and adding notebook in order to explain usage.

--- session.py
+++ session.py
@@ -1,36 +1,48 @@
+"""
+Integrate the Losswise service into the current project using a context manager.
+"""
+
+# EXT
 import losswise
-import time
-import random
 
 
-from contextlib import contextmanager
+class LWSession:
+    """
+    Context manager that creates a Losswise session that lets you log model parameters and performance.
+    If no API key is given, just do nothing (including not throwing any errors). Statements referring to any
+    Losswise functionalities can however still be left inside the code.
+    """
+    def __init__(self, api_key, tag, model_params, **session_params):
+        # Set key
+        if api_key is not None:
+            self.api_key_given = True
+            losswise.set_api_key(api_key)
 
+            # Create Session object
+            self.session = losswise.Session(tag=tag, params=model_params, **session_params)
+        else:
+            self.api_key_given = False
 
-@contextmanager
-def LWSession(api_key, tag, model_params, **session_params):
-    # Set key
-    losswise.set_api_key(api_key)
+        self.graphs = {}
 
-    # Create Session object
-    session = losswise.Session(tag=tag, params=model_params, **session_params)
+    def create_graph(self, *args, **kwargs):
+        """
+        Create a graph in which data will be plotted.
+        """
+        if self.api_key_given:
+            self.graphs[args[0]] = self.session.graph(*args, **kwargs)
 
-    try:
-        yield session
-    except Exception as ex:
-        print("Problem with session encountered: {}".format(str(ex)))
-    finally:
-        session.done()
+    def __enter__(self):
+        # Little hacky: Return the class itself when with block is entered. This way log() can be used and
+        # a function call can easily be ignored when no API is given without writing special subclasses inhering from
+        # losswise.session and losswise.graph
+        return self
 
+    def __exit__(self, type, value, traceback):
+        if self.api_key_given:
+            self.session.done()
 
-model_params = {'rnn_size': 512, "bla": "bla"}
-with LWSession(api_key="EA8Q382M5", tag="simple_session_test", model_params=model_params) as session:
-    graph = session.graph('loss', kind='min')
-    for x in range(10):
-        train_loss = 1. / (0.1 + x + 0.1 * random.random())
-        test_loss = 1.5 / (0.1 + x + 0.2 * random.random())
-        print(x)
-        graph.append(x, {'train_loss': train_loss, 'test_loss': test_loss})
-        time.sleep(5.)
-
-
-
+    def plot(self, *args, title=None):
+        if self.api_key_given:
+            assert title is not None, "Title of graph needs to be given."
+            self.graphs[title].append(*args)
