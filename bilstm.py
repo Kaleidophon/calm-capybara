1fba7f0262dcde31f084dcc7dd4cbf269b594233
Daniel <dfdazac@gmail.com>
2018-09-25 16:52:43 +0200

    Add dropout layers to BiLSTM

--- bilstm.py
+++ bilstm.py
@@ -7,26 +7,32 @@ from train import train_model
 from tweet_data import TweetsBaseDataset
 
 class biLSTMClassifier(nn.Module):
-    def __init__(self, embeddings, n_classes=20, dropout=0):
+    def __init__(self, embeddings, n_classes=20, emb_dropout=0,
+                 lstm_dropout=0):
         super(biLSTMClassifier, self).__init__()
+
         num_embeddings, embedding_dim = embeddings.shape
         self.embeddings = nn.Embedding(num_embeddings,
                                        embedding_dim,
                                        _weight=torch.from_numpy(embeddings))
+        self.emb_dropout = nn.Dropout(emb_dropout)
         self.lstm = nn.LSTM(embedding_dim, embedding_dim, bidirectional=True)
+        self.lstm_dropout = nn.Dropout(lstm_dropout)
         self.linear = nn.Linear(embedding_dim * 2, n_classes)
-        self.dropout = nn.Dropout(dropout)
 
     def forward(self, inputs, lengths):
         x = self.embeddings(inputs)
+        x = self.emb_dropout(x)
         x = pack_padded_sequence(x, lengths)
         _, (hidden, cell) = self.lstm(x)
-        # Concat the outputs of the both direction LSTM before passing it to the next layer
-        hidden = self.dropout(torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
 
-        # First dimension is sequence dimension which here always equals 1
-        # so we remove it before passing it to the linear layer
-        logits = self.linear(hidden)
+        # hidden has shape (2, batch_size, embedding_dim)
+        # Concat the outputs of the both direction LSTM
+        # before passing it to the next layer
+        x = torch.cat((hidden[0], hidden[1]), dim=1)
+
+        x = self.lstm_dropout(x)
+        logits = self.linear(x)
 
         return logits
 
@@ -46,6 +52,11 @@ if __name__ == '__main__':
 
     datasets = (train_set, dev_set, test_set)
 
-    metadata = {'Model name': 'Bidirectional LSTM'}
-    train_model(model, datasets, batch_size=32, epochs=20,
-                learning_rate=1e-3, metadata=metadata)
+    metadata = {'Model name': 'BiLSTM'}
+    val_score, test_score = train_model(model, datasets, batch_size=32,
+                                        epochs=20,
+                                        learning_rate=1e-3, metadata=metadata,
+                                        weights={
+                                            'linear': model.linear.weight.data})
+
+    print(val_score, test_score)
