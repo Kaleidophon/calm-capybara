c63b5546ec2deb53f8cb62bef5b8e6ae8da8d72f
Daniel <dfdazac@gmail.com>
2018-09-26 11:47:12 +0200

    Handle keyboard interruptions

--- train.py
+++ train.py
@@ -137,77 +137,82 @@ def train_model(model, datasets, batch_size, epochs, learning_rate,
     writer.add_text('metadata', text_summary)
 
     best_score = 0
+    test_f1 = 0
     best_ckpt_link = os.path.join(logdir, 'best-ckpt.pt')
 
-    steps = 0
-    for epoch in range(1, epochs + 1):
-        model.train()
-        print('Epoch {:d}/{:d}'.format(epoch, epochs))
-        n_batches = 0
-        for inputs, labels, lengths in train_loader:
-            steps += 1
-            n_batches += 1
+    try:
+        steps = 0
+        for epoch in range(1, epochs + 1):
+            model.train()
+            print('Epoch {:d}/{:d}'.format(epoch, epochs))
+            n_batches = 0
+            for inputs, labels, lengths in train_loader:
+                steps += 1
+                n_batches += 1
 
-            inputs = inputs.to(device)
-            labels = labels.to(device)
-            lengths = lengths.to(device)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                lengths = lengths.to(device)
 
-            # Initialize the gradients to zero
-            optimizer.zero_grad()
+                # Initialize the gradients to zero
+                optimizer.zero_grad()
 
-            # Run the model
-            outputs = model(inputs, lengths)
-            loss = criterion(outputs, labels)
+                # Run the model
+                outputs = model(inputs, lengths)
+                loss = criterion(outputs, labels)
 
-            # Optimize
-            loss.backward()
-            optimizer.step()
+                # Optimize
+                loss.backward()
+                optimizer.step()
 
-            # Log scores on training set
-            if n_batches % 100 == 0:
-                f1 = _get_score(outputs, labels)
-                print("\r{}/{}: loss = {:.4f}, f1_score = {:.4f}".format(
-                    n_batches, len(train_loader), loss, f1),
-                    end='', flush=True)
+                # Log scores on training set
+                if n_batches % 100 == 0:
+                    f1 = _get_score(outputs, labels)
+                    print("\r{}/{}: loss = {:.4f}, f1_score = {:.4f}".format(
+                        n_batches, len(train_loader), loss, f1),
+                        end='', flush=True)
 
-                # Write metrics to TensorBoard
-                writer.add_scalar('training/loss', loss, steps)
-                writer.add_scalar('training/f1_score', f1, steps)
-                # Write histograms
-                if weights is not None:
-                    for name, data in weights.items():
-                        writer.add_histogram('weights/' + name, data, steps)
+                    # Write metrics to TensorBoard
+                    writer.add_scalar('training/loss', loss, steps)
+                    writer.add_scalar('training/f1_score', f1, steps)
+                    # Write histograms
+                    if weights is not None:
+                        for name, data in weights.items():
+                            writer.add_histogram('weights/' + name, data, steps)
 
-        # Evaluate on dev set
-        eval_loss, eval_f1 = evaluate(model, criterion, dev_set)
-        print("\nvalidation loss = {:.4f}, validation f1_score = {:.4f}".format(
-            eval_loss, eval_f1))
+            # Evaluate on dev set
+            eval_loss, eval_f1 = evaluate(model, criterion, dev_set)
+            print("\nvalidation loss = {:.4f}, validation f1_score = {:.4f}".format(
+                eval_loss, eval_f1))
+
+            # Write to Tensorboard
+            writer.add_scalar('validation/loss', eval_loss, steps)
+            writer.add_scalar('validation/f1_score', eval_f1, steps)
+
+            # Save the checkpoint
+            ckpt_path = os.path.join(logdir, 'ckpt-{:d}.pt'.format(epoch))
+            save_model(model, optimizer, epoch, ckpt_path)
+
+            # Create a symbolic link to the best model
+            if eval_f1 > best_score:
+                best_score = eval_f1
+                if os.path.islink(best_ckpt_link):
+                    os.unlink(best_ckpt_link)
+                os.symlink(os.path.basename(ckpt_path), best_ckpt_link)
+
+        print("Training Completed. Evaluating on test set...")
+
+        # Evaluate on test set
+        test_loss, test_f1 = evaluate(model, criterion, test_set)
+        print("\ntest loss = {:.4f}, test f1_score = {:.4f}".format(
+            test_loss, test_f1))
 
         # Write to Tensorboard
-        writer.add_scalar('validation/loss', eval_loss, steps)
-        writer.add_scalar('validation/f1_score', eval_f1, steps)
+        writer.add_scalar('test/loss', test_loss, 0)
+        writer.add_scalar('test/f1_score', test_f1, 0)
 
-        # Save the checkpoint
-        ckpt_path = os.path.join(logdir, 'ckpt-{:d}.pt'.format(epoch))
-        save_model(model, optimizer, epoch, ckpt_path)
-
-        # Create a symbolic link to the best model
-        if eval_f1 > best_score:
-            best_score = eval_f1
-            if os.path.islink(best_ckpt_link):
-                os.unlink(best_ckpt_link)
-            os.symlink(os.path.basename(ckpt_path), best_ckpt_link)
-
-    print("Training Completed. Evaluating on test set...")
-
-    # Evaluate on test set
-    test_loss, test_f1 = evaluate(model, criterion, test_set)
-    print("\ntest loss = {:.4f}, test f1_score = {:.4f}".format(
-        test_loss, test_f1))
-
-    # Write to Tensorboard
-    writer.add_scalar('test/loss', test_loss, 0)
-    writer.add_scalar('test/f1_score', test_f1, 0)
+    except KeyboardInterrupt:
+        print('Interrupted training.')
 
     return best_score, test_f1
 
